9352213f1da062a2d2d83e770e04d618d1b12043
G4br13l4 <gabolf93@gmail.com>
2018-09-19 12:16:55 -0500

    get unique stores and count products of each

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './data.js';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state= {
       rawData:[],
-      products:""
+      stores:[]
     };
     this.getData = this.getData.bind(this);
+    this.getStores = this.getStores.bind(this);
   }
 
   componentDidMount() {
     this.getData();
+    this.getStores();
   }
 
   getData(){
     var xhr = new XMLHttpRequest();
     var json_obj, status = false;
-    xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://canastarosa.com/api/v1/market/products/?page_size=20&page=1", true);
+    xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://canastarosa.com/services/api/v1/market/products/?category__slug=infantil&page_size=200&page=1", true);
     xhr.onload = function (e) {
       if (xhr.readyState === 4) {
         if (xhr.status === 200) {
           var json_obj = JSON.parse(xhr.responseText);
           status = true;
-          console.log(json_obj)
+          this.setState({rawData:json_obj.results});
+          //this.getStores();
         } else {
           console.error(xhr.statusText);
         }
@@ -35,6 +39,48 @@ class App extends Component {
       console.error(xhr.statusText);
     };
     xhr.send(null);
+  }
+
+  getStores(){
+    //let allProducts = this.state.rawData;
+    let allProducts= data.results;
+    //get all the names of the stores
+    let allStores = allProducts.map(function(x) {
+        return x.store.name;
+    });
+
+    //get unique names of stores
+    const unique = (value, index, self) => {
+        //returns the first index at which a given element can be found in the array
+        return self.indexOf(value) === index;
+    }
+    const uniqueStores = allStores.filter(unique).sort();
+    this.countStores(uniqueStores, allStores);
+  }
+
+  countStores(uniqueStores, allStores){
+    let stores = uniqueStores.map(function(unique) {
+      let count=0;
+        allStores.forEach(function(element) {
+          if(unique===element){
+            count++
+          }
+        });
+      //create template of store
+      let store=(
+        <div>
+          <p>{unique}<span> ({count})</span></p>
+        </div>
+      );
+      return store;
+    });
+    //paint store
+    this.setState({stores:stores})
+  }
+
+  search(e){
+    e.preventDefault();
+    console.log("hello")
   }
 
   render() {
@@ -65,10 +111,18 @@ class App extends Component {
         <section>
           <div id="stores">
             Tiendas
+            {this.state.stores}
           </div>
-          <div id="products">
-            Productos
-            {this.state.products}
+          <div id="sub-nav">
+            <div>Productos</div>
+            <div>
+            <form onSubmit={this.search}>
+              <input type="text" name="search" placeholder="¿Qué estás buscando?"/>
+              <input type="submit"/>
+            </form>
+            </div>
+            <div>Ordenar por</div>
+            <section id="results"></section>
           </div>
         </section>
       </div>
